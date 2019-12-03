09b263dd9b40f7ac17c9590605e8aa8a46df1095
Alek Kruk <kruk.aleksander@hotmail.com>
2019-12-03 06:56:43 +0100

    use new table with numbers of pushups

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 let navi, weekName, target, targetList, exercises, time, goal, btn, timeVal, idI, activeNum, targetNr;
 domMaker(); // funkcja wiążąca dom z resztą
-// bazy z tekstami
 const weeks = {
     finalTargets: [13, 20, 28, 40, 50, 60],
     targets: [
@@ -12,57 +11,6 @@ const weeks = {
         [26, 26, 33, 33, 26, 26, 22, 22]
     ]
 }
-const weekDesc = {
-    week1: `<strong>Cel: </strong>Możesz zrobić co najmniej 13 pompek po zakończeniu 4
-serii, w których zrobiłeś kolejno 11, 12, 9 i 8 pompek. Przerwy między seriami trwają 45 sekund.`,
-    week2: `<strong>Cel: </strong>Możesz zrobić co najmniej 20 pompek po zakończeniu 4 serii, w których zrobiłeś kolejno 16, 17, 14 i 14 pompek. Przerwy między seriami trwają 45 sekund.`,
-    week3: `<strong>Cel: </strong>Możesz zrobić co najmniej 28 pompek po zakończeniu 4 serii, w których zrobiłeś kolejno 22, 30, 20 i 20 pompek. Przerwy między seriami trwają 45 sekund.`,
-    week4: `<strong>Cel: </strong>Możesz zrobić co najmniej 40 pompek po zakończeniu 4 serii, w których zrobiłeś kolejno 29, 33, 29 i 29 pompek. Przerwy między seriami trwają 45 sekund.`,
-    week5: `<strong>Cel: </strong>Możesz zrobić co najmniej 50 pompek po zakończeniu 7 serii, w których zrobiłeś kolejno 20, 20, 24, 24, 20, 20 i 22 pompki. Przerwy między seriami trwają 45 sekund.`,
-    week6: `<strong>Cel: </strong>Możesz zrobić co najmniej 60 pompek po zakończeniu 8 serii, w których zrobiłeś kolejno 26, 26, 33, 33, 26, 26, 22 i 22. Przerwy między seriami trwają 45 sekund.`
-}
-// obiek z zadaniami da danego tygodnia
-const targets = {
-    week1: `<span class="round">11</span>
-    <span class="round">12</span>
-    <span class="round">9</span>
-    <span class="round">8</span>
-    <span class="round">13</span>`,
-    week2: `<span class="round">16</span>
-    <span class="round">17</span>
-    <span class="round">14</span>
-    <span class="round">14</span>
-    <span class="round">20</span>`,
-    week3: `<span class="round">22</span>
-    <span class="round">30</span>
-    <span class="round">20</span>
-    <span class="round">20</span>
-    <span class="round">28</span>`,
-    week4: `<span class="round">29</span>
-    <span class="round">33</span>
-    <span class="round">29</span>
-    <span class="round">29</span>
-    <span class="round">40</span>`,
-    week5: `<span class="round">20</span>
-    <span class="round">20</span>
-    <span class="round">24</span>
-    <span class="round">24</span>
-    <span class="round">20</span>
-    <span class="round">20</span>
-    <span class="round">22</span>  
-    <span class="round">50</span>`,
-    week6: `<span class="round">26</span>
-    <span class="round">26</span>
-    <span class="round">33</span>
-    <span class="round">33</span>
-    <span class="round">26</span>
-    <span class="round">26</span>
-    <span class="round">22</span>
-    <span class="round">22</span>
-    <span class="round">60</span>`
-}
-// koniec bazy z tekstami
-
 
 function domMaker() {
     navi = [...document.querySelectorAll('li')]; // nawigacja
@@ -114,17 +62,22 @@ const start = () => { //funkcja samego stopera
     }
 }
 const setActive = () => { //robi cwiczenia na zielono
+    domMaker();
     exercises[activeNum].classList.add('active');
 }
 
 for (i = 0; i < navi.length; i++) { // zmienia nazwe tygodnia, opis, liste cwiczen
     navi[i].addEventListener('click', function () {
+        clearing();
         weekName.textContent = this.textContent;
         target.innerHTML = `<strong>Cel: </strong>Możesz zrobić co najmniej ${weeks["finalTargets"][this.dataset.nr]} pompek po zakończeniu 4
         serii, w których zrobiłeś kolejno ${weeks["targets"][this.dataset.nr].join(", ")} pompek. Przerwy między seriami trwają 45 sekund.`;
-        // ***********************************************************************************************
-        // to jest kod nad którym musisz popracować!       
-        targetList.innerHTML = targets[this.id];
+        let targetsToPush = []
+        weeks["targets"][this.dataset.nr].forEach(e => {
+            targetsToPush.push(`<span class="round">${e}</span>`)
+        });
+        targetsToPush.push(`<span class="round">${weeks['finalTargets'][this.dataset.nr]}</span>`);
+        targetList.innerHTML = targetsToPush.join("");
         goal.innerHTML = `Wykonaj <span class="am-nr"></span> pompek <span>.</span>
         <span>.</span>
         <span>.</span>`;
@@ -142,3 +95,9 @@ document.querySelector('.menu-mobile').addEventListener('click', () => {
     document.querySelector('.menu-mobile-cont').classList.add('active-menu');
 })
 // pamiętaj, że jak deklarujesz w funkcji zmienne to ona je tworzy we własnym scope
+
+
+// menu mobilne wyrzuca błąd w konsoli - usunąć,
+// ćwiczenie na dole - musi wiedzieć od początku, które pompki masz zrobić
+// ruszaj z scss
+// dodaj okienko w którym ładnie będzie widać ile jest pompek do zrobienia albo jakoś to wyraźnie oznacz.
